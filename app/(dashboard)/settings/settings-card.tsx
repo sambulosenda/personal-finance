b53cf509f0f47374ec3dc6a75d5e839f305bee20
Sambulo <sambulosendas@gmail.com>
2024-08-18 18:55:42 +0100

    chore: Update settings card to display loading state for connected bank account

--- app/(dashboard)/settings/settings-card.tsx
+++ app/(dashboard)/settings/settings-card.tsx
@@ -1,13 +1,35 @@
-"use-client";
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useGetConnectedBank } from "@/features/plaid/api/use-get-connected-bank";
 import { PlaidConnect } from "@/features/plaid/components/plaid-connect";
 import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
 
 export const SettingCard = () => {
-  const connectBank = null;
+  const { data: connectedBank, isLoading: isLoadingConnectedBank } =
+    useGetConnectedBank();
+
+  if (isLoadingConnectedBank) {
+    return (
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader>
+          <CardTitle className="text-xl line-clamp-1">
+            <Skeleton className="h-6 w-24" />
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="h-[350px] w-full flex items-center justify-center">
+            <Loader2 className="size-6 text-slate-300 animate-spin" />
+          </div>
+        </CardContent>
+      </Card>
+    );
+  }
+
   return (
     <Card className="border-none drop-shadow-sm">
       <CardHeader>
@@ -23,16 +45,15 @@ export const SettingCard = () => {
             <div
               className={cn(
                 "text-sm truncate flex items-center ",
-                !connectBank && "text-muted-foreground"
+                !connectedBank && "text-muted-foreground"
               )}
             >
-              {connectBank
+              {connectedBank
                 ? "Back account connected"
                 : "No bank account connected"}
             </div>
 
             <PlaidConnect />
-
           </div>
         </div>
       </CardContent>
